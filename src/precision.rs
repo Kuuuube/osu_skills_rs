876fce9271bc4cafeb6ece0c126ffb545edb06cd
kuuube <hexagonisalie@gmail.com>
2022-11-27 22:54:42 -0500

    Calculate agility_subtract instead of using set value

--- src/precision.rs
+++ src/precision.rs
@@ -1,12 +1,13 @@
-pub fn calculate_precision(agility: f64, agility_limit: f64, agility_pow: f64, agility_subtract: f64, beatmap_cs: i8, total_mult: f64, total_pow: f64) -> f64 {
-        let scaled_agility;
-    
-        if agility > agility_limit {
-            scaled_agility = 1.0;
-        } else {
-            scaled_agility = f64::powf(agility + 1.0, agility_pow) - agility_subtract;
-        }
-    
-        let precision: f64 = total_mult * f64::powf(scaled_agility * beatmap_cs as f64, total_pow);
-        return precision;
+pub fn calculate_precision(agility: f64, agility_limit: f64, agility_pow: f64, beatmap_cs: i8, total_mult: f64, total_pow: f64) -> f64 {
+    let scaled_agility;
+    let agility_subtract = f64::powf(agility_limit + 1.0, 0.1) - 1.0;
+
+    if agility > agility_limit {
+        scaled_agility = 1.0;
+    } else {
+        scaled_agility = f64::powf(agility + 1.0, agility_pow) - agility_subtract;
     }
+
+    let precision: f64 = total_mult * f64::powf(scaled_agility * beatmap_cs as f64, total_pow);
+    return precision;
+}
