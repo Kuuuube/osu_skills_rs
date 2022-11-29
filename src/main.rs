e3388c0116a249bb4a3326e82021c2391a0ca94b
kuuube <hexagonisalie@gmail.com>
2022-11-28 20:25:31 -0500

    Use structs in precision calc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 mod precision;
+mod structs;
+mod vector2d;
 
 fn main() {
-    let precision = precision::calculate_precision(223.217, 1000.0, 0.1, 10, 20.0, 2.0).to_string();
+    let testmap: structs::Beatmap = Default::default();
+
+    let precision = precision::calculate_precision(testmap, 1000.0, 0.1, 20.0, 2.0).to_string();
     println!("new: {precision}");
+
+    let test = vector2d::Vector2F64{ x: 100.0, y: 500.2};
+
+    println!("{}, {}", test.x, test.y);
 }
--- src/precision.rs
+++ src/precision.rs
@@ -1,14 +1,16 @@
-pub fn calculate_precision(agility: f64, agility_limit: f64, agility_pow: f64, beatmap_cs: i8, total_mult: f64, total_pow: f64) -> f64 {
+use crate::structs;
+
+pub fn calculate_precision(beatmap: structs::Beatmap, agility_limit: f64, agility_pow: f64, total_mult: f64, total_pow: f64) -> f64 {
     let scaled_agility;
     let agility_subtract = f64::powf(agility_limit, 0.1) - 1.0;
     let agility_shift = f64::powf(agility_subtract, 10.0);
 
-    if agility > agility_limit {
+    if beatmap.skills.agility > agility_limit {
         scaled_agility = 1.0;
     } else {
-        scaled_agility = f64::powf(agility + agility_shift, agility_pow) - agility_subtract;
+        scaled_agility = f64::powf(beatmap.skills.agility + agility_shift, agility_pow) - agility_subtract;
     }
 
-    let precision: f64 = total_mult * f64::powf(scaled_agility * beatmap_cs as f64, total_pow);
+    let precision: f64 = total_mult * f64::powf(scaled_agility * beatmap.cs, total_pow);
     return precision;
 }
