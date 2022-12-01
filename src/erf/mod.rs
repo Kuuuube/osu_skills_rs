50a44e30fadeb7d8de25584fa1a301bf6babe364
kuuube <hexagonisalie@gmail.com>
2022-12-01 13:46:51 -0500

    Add note to erf/mod.rs

--- src/erf/mod.rs
+++ src/erf/mod.rs
@@ -1,2 +1,6 @@
+//erf code taken from https://github.com/statrs-dev/statrs
+//unused code in erf.rs was commented out to avoid warning spam
+//uncomment the code if other variants of erf function are needed
+
 pub mod erf;
 pub mod evaluate;
