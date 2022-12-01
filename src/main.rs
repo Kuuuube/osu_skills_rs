8e011f5a6a2a5a335c346a1c61cb1b94d2a95d10
kuuube <hexagonisalie@gmail.com>
2022-12-01 14:45:12 -0500

    Add memory calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 mod utils;
 mod structs;
 mod pair_structs;
@@ -7,13 +9,17 @@ mod skill_calculation;
 
 fn main() {
     let mut testmap: structs::Beatmap = Default::default();
+ 
+/*     let hit_object_test: structs::HitObject = Default::default();
+    testmap.hit_objects = vec![hit_object_test];  */
 
     testmap.skills.precision = skill_calculation::precision::calculate_precision(&testmap);
-    testmap.skills.reaction = skill_calculation::reaction::calculate_reaction(&testmap, false);
+    testmap.skills.reaction = skill_calculation::reaction::calculate_reaction(&testmap);
     testmap.skills.stamina = skill_calculation::stamina::calculate_stamina(&testmap);
     testmap.skills.tenacity = skill_calculation::tenacity::calculate_tenacity(&testmap);
     testmap.skills.agility = skill_calculation::agility::calculate_agility(&testmap);
     testmap.skills.accuracy = skill_calculation::accuracy::calculate_accuracy(&testmap);
+    testmap.skills.memory = skill_calculation::memory::calculate_memory(&testmap);
 
     let precision = testmap.skills.precision.to_string();
     let reaction = testmap.skills.reaction.to_string();
@@ -21,5 +27,6 @@ fn main() {
     let tenacity = testmap.skills.tenacity.to_string();
     let agility = testmap.skills.agility.to_string();
     let accuracy = testmap.skills.accuracy.to_string();
-    println!("stamina: {stamina}, tenacity: {tenacity}, agility: {agility}, accuracy: {accuracy}, precision: {precision}, reaction: {reaction}, memory: {{memory}}")
+    let memory = testmap.skills.memory.to_string();
+    println!("stamina: {stamina}, tenacity: {tenacity}, agility: {agility}, accuracy: {accuracy}, precision: {precision}, reaction: {reaction}, memory: {memory}")
 }
---
+++ src/skill_calculation/memory.rs
@@ -0,0 +1,118 @@
+use crate::structs::HitObject;
+use crate::utils;
+use crate::structs;
+use crate::pair_structs;
+
+fn get_approach_relative_size(time: f64, hit_time: f64, ar: f64) -> f64 {
+    let ar_ms: f64 = utils::ar_to_ms(ar);
+    
+    if hit_time < time {
+        return 1.0;
+    } else if hit_time - ar_ms > time {
+        return 0.0;
+    } else {
+        return 1.0 + 3.0 * ((hit_time - time) / ar_ms);
+    }
+}
+
+fn is_observable_from(obj: &HitObject, distance: i32, from_pos: &pair_structs::Pairf64) -> bool {
+    let dist: f64 = pair_structs::get_distance_from(&obj.pos, &from_pos);
+    if dist < distance as f64 {
+        return true;
+    }
+    return false;
+}
+
+pub fn calculate_memory(beatmap: &structs::Beatmap) -> f64 {
+    if beatmap.hit_objects.len() > 0 {
+        let mut total_mem_points: f64 = 0.0;
+        let mut old: &HitObject = &beatmap.hit_objects[0];
+        let mut combo: i32 = 0;
+
+        let mut i: i32 = 1;
+        while i < beatmap.hit_objects.len() as i32 {
+            let cur: &HitObject = &beatmap.hit_objects[i as usize];
+            let mut mem_points: f64 = 0.0;
+            let observable_dist: i32;
+            if combo < 100 {
+                observable_dist = 160;
+            } else if combo < 200 {
+                observable_dist = 120;
+            } else {
+                observable_dist = 100;
+            }
+
+            let mut slider_bonus_factor: f64 = 1.0;
+
+            if utils::is_hit_object_type(&old.hit_object_type, structs::HitObjectType::Slider) {
+                slider_bonus_factor = 1.1; //this value comes from osu skills config file "SliderBuff"
+            }
+
+            let mut observable: bool = false;
+            let mut help_pixels: i32;
+
+            let mut j = i;
+            while j > 0 {
+                let prev: &structs::HitObject = &beatmap.hit_objects[j as usize];
+                if cur.time - prev.time > utils::ar_to_ms(beatmap.ar) as i64 {
+                    break;
+                }
+                if !utils::has_mod(&beatmap, structs::Mods::HD) {
+                    let size: f64 = get_approach_relative_size(prev.end_time as f64, cur.time as f64, beatmap.ar);
+                    help_pixels = (size * utils::cs_to_px(beatmap.cs)) as i32;
+                } else {
+                    let observable_time: i64 = cur.time - (utils::ar_to_ms(beatmap.ar) * 0.3) as i64;
+                    if prev.time > observable_time {
+                        continue;
+                    }
+                    help_pixels = utils::cs_to_px(beatmap.cs) as i32;
+                }
+                if is_observable_from(cur, observable_dist + help_pixels, &prev.pos) {
+                    observable = true;
+                    break;
+                }
+                j -= 1;
+            }
+
+            if !observable {
+                if !utils::has_mod(&beatmap, structs::Mods::HD) {
+                    let size: f64 = get_approach_relative_size(old.end_time as f64, cur.time as f64, beatmap.ar);
+                    help_pixels = (size * utils::cs_to_px(beatmap.cs)) as i32;
+                } else {
+                    help_pixels = utils::cs_to_px(beatmap.cs) as i32;
+                }
+
+                if utils::is_hit_object_type(&cur.hit_object_type, structs::HitObjectType::NewCombo) || utils::is_hit_object_type(&cur.hit_object_type, structs::HitObjectType::ColourHax) {
+                    let dist: f64 = pair_structs::get_distance_from(&cur.pos, &old.endpoint);
+                    if dist > (observable_dist + help_pixels) as f64 {
+                        mem_points = slider_bonus_factor * (dist / (cur.time - old.time) as f64);
+                    }
+                } else {
+                    let dist: f64 = pair_structs::get_distance_from(&cur.pos, &old.endpoint);
+                    if dist > (observable_dist + help_pixels) as f64 {
+                        let follow_points_nerf: f64 = 0.8; //this value comes from osu skills config file "FollowpointsNerf"
+                        mem_points = slider_bonus_factor * follow_points_nerf * (dist / (cur.time - old.time) as f64);
+                    }
+                }
+            }
+
+            if utils::is_hit_object_type(&cur.hit_object_type, structs::HitObjectType::Normal) || utils::is_hit_object_type(&cur.hit_object_type, structs::HitObjectType::Spinner) {
+                combo += 1;
+            } else if utils::is_hit_object_type(&cur.hit_object_type, structs::HitObjectType::Slider) {
+                combo += (cur.ticks.len() + 2) as i32;
+            }
+
+            old = cur;
+            total_mem_points += mem_points;
+
+            i += 1;
+        }
+    
+        let total_mult: f64 = 205.0; //this value comes from osu skills config file "TotalMult"
+        let total_pow: f64 = 0.3; //this value comes from osu skills config file "TotalPow"
+        let memory: f64 = total_mult * f64::powf(total_mem_points, total_pow);
+        
+        return memory;
+    }
+    return 0.0;
+}
