cda14f5b5895221f68e1f62825bb5acaff8e9096
kuuube <hexagonisalie@gmail.com>
2022-12-01 10:44:19 -0500

    Add tenacity calculation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod structs;
 mod pair_structs;
 mod patterns;
 mod stamina;
+mod tenacity;
 
 fn main() {
     let mut testmap: structs::Beatmap = Default::default();
@@ -12,9 +13,11 @@ fn main() {
     testmap.skills.precision = precision::calculate_precision(&testmap);
     testmap.skills.reaction = reaction::calculate_reaction(&testmap, false);
     testmap.skills.stamina = stamina::calculate_stamina(&testmap);
+    testmap.skills.tenacity = tenacity::calculate_tenacity(&testmap);
 
     let precision = testmap.skills.precision.to_string();
     let reaction = testmap.skills.reaction.to_string();
     let stamina = testmap.skills.stamina.to_string();
-    println!("precision: {precision}, reaction: {reaction}, stamina: {stamina}")
+    let tenacity = testmap.skills.tenacity.to_string();
+    println!("precision: {precision}, reaction: {reaction}, stamina: {stamina}, tenacity: {tenacity}")
 }
