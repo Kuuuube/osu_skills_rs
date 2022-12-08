3730564d8694936247350d22ab306bab1d9ecfcc
kuuube <hexagonisalie@gmail.com>
2022-12-08 16:37:28 -0500

    Split beatmap processing out of main fn in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,23 @@ mod skill_calculation;
 mod osu_parser;
 
 fn main() {
-    let mut beatmap: structs::Beatmap = osu_parser::parse_beatmap("./testmaps/v14.osu");
+    let beatmap: structs::Beatmap = process_beatmap("./testmaps/v14.osu");
 
-     if beatmap.hit_objects.len() >= 2 {
+    let reaction = beatmap.skills.reaction.to_string();
+    let stamina = beatmap.skills.stamina.to_string();
+    let tenacity = beatmap.skills.tenacity.to_string();
+    let agility = beatmap.skills.agility.to_string();
+    let precision = beatmap.skills.precision.to_string();
+    let accuracy = beatmap.skills.accuracy.to_string();
+    let memory = beatmap.skills.memory.to_string();
+    
+    println!("stamina: {stamina}, tenacity: {tenacity}, agility: {agility}, accuracy: {accuracy}, precision: {precision}, reaction: {reaction}, memory: {memory}");
+}
+
+fn process_beatmap(filepath_str: &str) -> structs::Beatmap {
+    let mut beatmap: structs::Beatmap = osu_parser::parse_beatmap(filepath_str);
+
+    if beatmap.hit_objects.len() >= 2 {
         beatmap = utils::apply_mods(beatmap);
 
         beatmap = skill_calculation::generic::prepare_timing_points(beatmap);
@@ -18,11 +32,11 @@ fn main() {
 
         beatmap = skill_calculation::generic::prepare_aim_data(beatmap);
         beatmap = skill_calculation::generic::prepare_tap_data(beatmap);
+        beatmap = skill_calculation::generic::find_patterns(beatmap);
+        beatmap = skill_calculation::generic::analyze_patterns(beatmap);
 
         beatmap = skill_calculation::strains::calculate_aim_strains(beatmap);
         beatmap = skill_calculation::strains::calculate_tap_strains(beatmap);
-        beatmap = skill_calculation::generic::find_patterns(beatmap);
-        beatmap = skill_calculation::generic::analyze_patterns(beatmap);
 
         beatmap.skills.reaction = skill_calculation::reaction::calculate_reaction(&beatmap);
         beatmap.skills.stamina = skill_calculation::stamina::calculate_stamina(&beatmap);
@@ -31,15 +45,6 @@ fn main() {
         beatmap.skills.precision = skill_calculation::precision::calculate_precision(&beatmap);
         beatmap.skills.accuracy = skill_calculation::accuracy::calculate_accuracy(&beatmap);
         beatmap.skills.memory = skill_calculation::memory::calculate_memory(&beatmap);
-
-        let precision = beatmap.skills.precision.to_string();
-        let reaction = beatmap.skills.reaction.to_string();
-        let stamina = beatmap.skills.stamina.to_string();
-        let tenacity = beatmap.skills.tenacity.to_string();
-        let agility = beatmap.skills.agility.to_string();
-        let accuracy = beatmap.skills.accuracy.to_string();
-        let memory = beatmap.skills.memory.to_string();
-        
-        println!("stamina: {stamina}, tenacity: {tenacity}, agility: {agility}, accuracy: {accuracy}, precision: {precision}, reaction: {reaction}, memory: {memory}")
     }
+    return beatmap;
 }
