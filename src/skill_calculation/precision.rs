c3e103f4293b7111112c93916488d05232cc2ab6
kuuube <hexagonisalie@gmail.com>
2022-12-09 15:05:46 -0500

    Fix classic precision calc

--- src/skill_calculation/precision.rs
+++ src/skill_calculation/precision.rs
@@ -21,21 +21,13 @@ pub fn calculate_precision(beatmap: &structs::Beatmap) -> f64 {
 }
 
 pub fn calculate_classic_precision(beatmap: &structs::Beatmap) -> f64 {
-    let agility_limit: f64 = 700.0; //this value comes from osu skills config file "AgilityLimit"
     let agility_pow: f64 = 0.1; //this value comes from osu skills config file "AgilityPow"
     let total_mult: f64 = 20.0; //this value comes from osu skills config file "TotalMult"
     let total_pow: f64 = 2.0; //this value comes from osu skills config file "TotalPow"
     let agility_subtract: f64 = 0.995462; //this value comes from osu skills config file "AgilitySubtract"
 
-    let scaled_agility: f64;
+    let scaled_agility: f64 = f64::powf(beatmap.skills.agility + 1.0, agility_pow) - agility_subtract;
 
-    if beatmap.skills.agility > agility_limit {
-        scaled_agility = 1.0;
-    } else {
-        scaled_agility = f64::powf(beatmap.skills.agility + 1.0, agility_pow) - agility_subtract;
-    }
-
-    let agility: f64 = total_mult * f64::powf(scaled_agility * beatmap.cs, total_pow);
-
-    return agility;
+    let precision: f64 = total_mult * f64::powf(scaled_agility * beatmap.cs, total_pow);
+    return precision;
 }
