5d5bfd2c0fcbb2e464c99a91fb747db25578fc4c
kuuube <hexagonisalie@gmail.com>
2022-12-10 11:13:28 -0500

    Fix accuracy calculation

--- src/skill_calculation/accuracy.rs
+++ src/skill_calculation/accuracy.rs
@@ -10,7 +10,7 @@ pub fn calculate_accuracy(beatmap: &structs::Beatmap) -> f64 {
         }
     }
 
-    let mut od_ms: f64 = utils::od_to_ms(beatmap.od);
+    let mut od_ms: f64 = utils::od_to_ms(beatmap.od as i32 as f64);
     if utils::has_mod(&beatmap, structs::Mods::DT) {
         od_ms /= 1.5;
     } else if utils::has_mod(&beatmap, structs::Mods::HT) {
@@ -20,13 +20,13 @@ pub fn calculate_accuracy(beatmap: &structs::Beatmap) -> f64 {
     let tapping: f64;
     let acc_scale: f64 = 0.01; //this value comes from osu skills config file "AccScale"
     if beatmap.skills.stamina == 0.0 {
-        tapping = 1.0; //erf::erf::erf(f64::INFINITY)
+        tapping = 1.0; //erf::erf(f64::INFINITY)
     } else {
-        tapping = erf::erf(od_ms / acc_scale * beatmap.skills.stamina * beatmap.skills.stamina);
+        tapping = erf::erf(od_ms / (acc_scale * beatmap.skills.stamina * beatmap.skills.stamina));
     }
 
     let verscale: f64 = 0.3; //this value comes from osu skills config file "VerScale"
-    let accuracy: f64 = verscale * circles * f64::log10(tapping);
+    let accuracy: f64 = -verscale * circles * f64::ln(tapping);
 
     return accuracy;
 }
