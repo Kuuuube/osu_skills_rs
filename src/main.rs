4845e0c59e9aca3f5aed08fd5c2e1c1dcc922915
kuuube <hexagonisalie@gmail.com>
2022-12-13 10:18:42 -0500

    Fix --out indentation for --help

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
         };
         let split: Vec<&str> = arg.split("=").collect();
         match &split[0].to_lowercase() as &str {
-            "--help" => { print!("osu!Skills rs\nUsage: osu_skills_rs [OPTION]...\n\nMandatory:\n     --file=FILE                 path to .osu file to parse\n\nOptional:\n     --alg=ALG                   calculation alg to use (`classic` or `default`)\n     --mods=MODS                 integer sum of all mod values to apply (`2`: EZ, `8`: HD, `16`: HR, `64`: DT, `256`: HT)\n     --is-dir                    set FILE to DIR and parse all .osu files in DIR\n--out=FILE                  output to FILE\n"); return }
+            "--help" => { print!("osu!Skills rs\nUsage: osu_skills_rs [OPTION]...\n\nMandatory:\n     --file=FILE                 path to .osu file to parse\n\nOptional:\n     --alg=ALG                   calculation alg to use (`classic` or `default`)\n     --mods=MODS                 integer sum of all mod values to apply (`2`: EZ, `8`: HD, `16`: HR, `64`: DT, `256`: HT)\n     --is-dir                    set FILE to DIR and parse all .osu files in DIR\n     --out=FILE                  output to FILE\n"); return }
             "--file" => { path = safe_get_string(split, 1) },
             "--alg" => { alg = safe_get_string(split, 1) },
             "--mods" => { mod_int = safe_parse_i32(safe_get_string(split, 1)) },
