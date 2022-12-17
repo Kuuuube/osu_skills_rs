ad67f4c094e88571189edbea9e813693cb12342e
kuuube <hexagonisalie@gmail.com>
2022-12-17 12:41:13 -0500

    Add skill file parser

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod structs;
 mod pair_structs;
 mod osu_parser;
 mod output;
+mod parser;
 
 fn main() {
     panic::set_hook(Box::new(|_info| {
@@ -16,13 +17,15 @@ fn main() {
 
     let args: Vec<String> = std::env::args().collect();
 
-    let mut osu_filepath: String = Default::default();
+    let mut input_filepath: String = Default::default();
     let mut alg: String = Default::default();
     let mut mod_int: i32 = Default::default();
     let mut is_dir: String = Default::default();
-    let mut output_file_string: String = Default::default();
+    let mut output_filepath: String = Default::default();
     let mut output_type: String = "stdout".to_string();
     let mut no_ext: bool = false;
+    let mut parser_mode: bool = false;
+    let mut parser_arg: String = Default::default();
 
     let mut i: usize = 1;
     while i < args.len() {
@@ -32,31 +35,38 @@ fn main() {
         };
         let split: Vec<&str> = arg.split("=").collect();
         match &split[0].to_lowercase() as &str {
-            "--help" => { print!("osu!Skills rs\nUsage: osu_skills_rs [OPTION]...\n\nMandatory:\n     --file=FILE                 path to .osu file to parse\n\nOptional:\n     --alg=ALG                   calculation alg to use (classic|default)\n     --mods=MODS                 integer sum of all mod values to apply (`2`: EZ|`8`: HD|`16`: HR|`64`: DT|`256`: HT)\n     --is-dir=TYPE               set FILE to DIR or SUBDIR (recursive) and parse all .osu files in (DIR|SUBDIR)\n     --output-type=TYPE          output stream and type (stdout|file-txt|file-csv)\n     --out=FILE                  set output FILE (output-type must be file-txt or file-csv)\n     --no-ext                    removes file extension check for .osu files\n"); return }
-            "--file" => { osu_filepath = safe_get_string(split, 1) },
+            "--help" => { print!("
+            osu!Skills rs\nUsage: osu_skills_rs [OPTION]...\n\nSkill Calculator:\n  Mandatory:\n     --in=FILE                   path to .osu file to parse\n\n  Optional:\n     --alg=ALG                   calculation alg to use (classic|default)\n     --mods=MODS                 integer sum of all mod values to apply (`2`: EZ|`8`: HD|`16`: HR|`64`: DT|`256`: HT)\n     --is-dir=TYPE               set FILE to DIR or SUBDIR (recursive) and parse all .osu files in (DIR|SUBDIR)\n     --output-type=TYPE          output stream and type (stdout|file-txt|file-csv)\n     --out=FILE                  set output FILE (output-type must be file-txt or file-csv)\n     --no-ext                    removes file extension check for .osu files\n\nSkill File Parser:\n  Mandatory:\n     --parser=ARGS               args for the parser in the following format:\n                                 collections separated by `;`, filters separated by `,`\n                                 fiters separated from values by `:`, min and max values separated by `-`\n                                 example: \"stamina:1-100,tenacity:100-200;precision:900-1000\"\n     --in=FILE                   input file\n     --out=FILE                  output file\n"); return }
+            "--in" => { input_filepath = safe_get_string(split, 1) },
             "--alg" => { alg = safe_get_string(split, 1) },
             "--mods" => { mod_int = safe_parse_i32(safe_get_string(split, 1)) },
             "--is-dir" => { is_dir = safe_get_string(split, 1) },
-            "--out" => { output_file_string = safe_get_string(split, 1) },
+            "--out" => { output_filepath = safe_get_string(split, 1) },
             "--output-type" => { output_type = safe_get_string(split, 1) },
             "--no-ext" => { no_ext = true },
+            "--parser" => { parser_mode = true; parser_arg = safe_get_string(split, 1)},
             _ => { print!("osu!Skills rs: unknown option {}\nUsage: osu_skills_rs [OPTION]...\n\nTry `osu_skills_rs --help` for more options.\n", split[0]); return }
         }
 
         i += 1;
     }
 
-    if osu_filepath.len() == 0 {
+    if parser_mode {
+        parser::skill_file_parser(parser_arg, input_filepath, output_filepath);
+        return;
+    }
+
+    if input_filepath.len() == 0 {
         print!("osu!Skills rs: missing .osu file path\nUsage: osu_skills_rs [OPTION]...\n\nTry `osu_skills_rs --help` for more options.\n");
         return;
     }
 
     let mut files: Vec<std::path::PathBuf> = Default::default();
 
-    print!("Collecting filenames from `{}`\n", osu_filepath);
+    print!("Collecting filenames from `{}`\n", input_filepath);
     match &is_dir.to_lowercase() as &str {
         "dir" => {
-            let paths = match fs::read_dir(osu_filepath) {
+            let paths = match fs::read_dir(input_filepath) {
                 Ok(ok) => ok,
                 Err(_) => return
             };
@@ -75,7 +85,7 @@ fn main() {
         },
         "subdir" => {
             let mut dirs: Vec<std::path::PathBuf> = Default::default();
-            let paths = match fs::read_dir(osu_filepath) {
+            let paths = match fs::read_dir(input_filepath) {
                 Ok(ok) => ok,
                 Err(_) => return
             };
@@ -117,14 +127,14 @@ fn main() {
                 i += 1;
             }
         },
-        _ => files.push(std::path::Path::new(&osu_filepath).to_path_buf())
+        _ => files.push(std::path::Path::new(&input_filepath).to_path_buf())
     };
 
     print!("Starting calculation of `{}` maps\n", files.len());
     match &output_type.to_lowercase() as &str {
         "stdout" => { output::output_stdout(mod_int, alg, files) },
-        "file-txt" => { output::output_file_txt(mod_int, alg, files, output_file_string) },
-        "file-csv" => { output::output_file_csv(mod_int, alg, files, output_file_string) },
+        "file-txt" => { output::output_file_txt(mod_int, alg, files, output_filepath) },
+        "file-csv" => { output::output_file_csv(mod_int, alg, files, output_filepath) },
         _ => {}
     }
 
---
+++ src/parser.rs
@@ -0,0 +1,294 @@
+//arg format:
+//"name:collect1,stamina:1-100,tenacity:100-200;name:collect2,agility:50-90;name:collect1,memory:200-300;precision:900-1000"
+//^                                             ^                           ^                            ^
+//collection 1                                  collection 2                collection 1                 collection 3
+
+use std::io::{BufRead, Write};
+
+pub fn skill_file_parser(arg: String, input_filepath: String, output_filepath: String) {
+    let mut collections: Vec<Collection> = read_arg(arg);
+    let beatmaps: Vec<BeatmapData> = read_maps(input_filepath);
+
+    let mut i: usize = 0;
+    while i < beatmaps.len() {
+        let mut j: usize = 0;
+        while j < collections.len() {
+            if collections[j].collection_filter.stamina_max >= beatmaps[i].stamina && collections[j].collection_filter.stamina_min <= beatmaps[i].stamina &&
+            collections[j].collection_filter.tenacity_max >= beatmaps[i].tenacity && collections[j].collection_filter.tenacity_min <= beatmaps[i].tenacity &&
+            collections[j].collection_filter.agility_max >= beatmaps[i].agility && collections[j].collection_filter.agility_min <= beatmaps[i].agility &&
+            collections[j].collection_filter.accuracy_max >= beatmaps[i].accuracy && collections[j].collection_filter.accuracy_min <= beatmaps[i].accuracy &&
+            collections[j].collection_filter.precision_max >= beatmaps[i].precision && collections[j].collection_filter.precision_min <= beatmaps[i].precision &&
+            collections[j].collection_filter.reaction_max >= beatmaps[i].reaction && collections[j].collection_filter.reaction_min <= beatmaps[i].reaction &&
+            collections[j].collection_filter.memory_max >= beatmaps[i].memory && collections[j].collection_filter.memory_min <= beatmaps[i].memory {
+                collections[j].beatmap_data.push(beatmaps[i].clone());
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+
+    write_collectionconverter_csv(collections, output_filepath);
+
+}
+
+fn write_collectionconverter_csv(collections: Vec<Collection>, output_filepath: String) {
+    //format: "CollectionName","MapId","MapSetId","Md5","PlayMode","ArtistRoman","ArtistUnicode","TitleRoman","TitleUnicode","DiffName","StarsNomod"
+
+    let mut output_file = match std::fs::OpenOptions::new().create(true).write(true).truncate(true).open(output_filepath) {
+        Ok(ok) => ok,
+        Err(error) => panic!("osu!Skills rs: couldn't open file. Error: {}", error)
+    };
+
+    match output_file.write(("\"CollectionName\",\"MapId\",\"MapSetId\",\"Md5\",\"PlayMode\",\"ArtistRoman\",\"ArtistUnicode\",\"TitleRoman\",\"TitleUnicode\",\"DiffName\",\"StarsNomod\"\n").as_bytes()) {
+        Ok(_) => (),
+        Err(error) => println!("osu!Skills rs: failed to write file. Error: {}\n\n", error)
+    };
+
+    for collection in collections {
+        let name: String;
+            if collection.collection_name == String::default() {
+                name = collection.default_name;
+            } else {
+                name = collection.collection_name;
+            }
+
+        for beatmap in collection.beatmap_data {
+            let formatted_string: String = format!("{},{},{},{},{},{},{},{},{},{},{}\n", name, beatmap.beatmap_id, beatmap.beatmap_set_id, beatmap.beatmap_md5, "Osu", beatmap.artist, beatmap.artist_unicode, beatmap.title, beatmap.title_unicode, beatmap.version, -1);
+            match output_file.write(formatted_string.as_bytes()) {
+                Ok(_) => (),
+                Err(error) => println!("osu!Skills rs: failed to write file. Error: {}\n\n", error)
+            };
+        }
+    }
+
+}
+
+fn read_arg(arg: String) -> Vec<Collection> {
+    let mut char_vec: Vec<char> = Default::default();
+    let illegal_chars: Vec<char> = vec!['\"', '\''];
+    for char in arg.chars() {
+        if !illegal_chars.contains(&char) {
+            char_vec.push(char);
+        }
+    }
+    let arg_trimmed: String = char_vec.into_iter().collect();
+    let args_per_collection: Vec<&str> = arg_trimmed.split(";").collect();
+
+    let mut collections: Vec<Collection> = vec![Collection::default(); args_per_collection.len()];
+
+    let mut i: usize = 0;
+    while i < args_per_collection.len() {
+        let args_per_type: Vec<&str> = args_per_collection[i].split(",").collect();
+        
+        for arg_type in args_per_type {
+            let args_per_value: Vec<&str> = arg_type.split(":").collect();
+            let value_1: String = safe_get_string(&args_per_value, 1);
+            let split_min_max: Vec<&str> = value_1.split("-").collect();
+            match &args_per_value[0].to_lowercase() as &str {
+                "name" => { collections[i].collection_name = safe_get_string(&args_per_value, 1) },
+                "stamina" => {
+                    collections[i].collection_filter.stamina_min = safe_parse_f64(safe_get_string(&split_min_max, 0));
+                    collections[i].collection_filter.stamina_max = safe_parse_f64(safe_get_string(&split_min_max, 1));
+                    if collections[i].default_name == String::default() {
+                        collections[i].default_name = format!("{}_{}-{}", args_per_value[0], collections[i].collection_filter.stamina_min, collections[i].collection_filter.stamina_max);
+                    } else {
+                        collections[i].default_name = format!("{}_{}_{}-{}", collections[i].default_name, args_per_value[0], collections[i].collection_filter.stamina_min, collections[i].collection_filter.stamina_max);
+                    }
+                },
+                "tenacity"|"streams" => {
+                    collections[i].collection_filter.tenacity_min = safe_parse_f64(safe_get_string(&split_min_max, 0));
+                    collections[i].collection_filter.tenacity_max = safe_parse_f64(safe_get_string(&split_min_max, 1));
+                    if collections[i].default_name == String::default() {
+                        collections[i].default_name = format!("{}_{}-{}", args_per_value[0], collections[i].collection_filter.tenacity_min, collections[i].collection_filter.tenacity_max);
+                    } else {
+                        collections[i].default_name = format!("{}_{}_{}-{}", collections[i].default_name, args_per_value[0], collections[i].collection_filter.tenacity_min, collections[i].collection_filter.tenacity_max);
+                    }
+                },
+                "agility"|"aim" => {
+                    collections[i].collection_filter.agility_min = safe_parse_f64(safe_get_string(&split_min_max, 0));
+                    collections[i].collection_filter.agility_max = safe_parse_f64(safe_get_string(&split_min_max, 1));
+                    if collections[i].default_name == String::default() {
+                        collections[i].default_name = format!("{}_{}-{}", args_per_value[0], collections[i].collection_filter.agility_min, collections[i].collection_filter.agility_max);
+                    } else {
+                        collections[i].default_name = format!("{}_{}_{}-{}", collections[i].default_name, args_per_value[0], collections[i].collection_filter.agility_min, collections[i].collection_filter.agility_max);
+                    }
+                },
+                "accuracy" => {
+                    collections[i].collection_filter.accuracy_min = safe_parse_f64(safe_get_string(&split_min_max, 0));
+                    collections[i].collection_filter.accuracy_max = safe_parse_f64(safe_get_string(&split_min_max, 1));
+                    if collections[i].default_name == String::default() {
+                        collections[i].default_name = format!("{}_{}-{}", args_per_value[0], collections[i].collection_filter.accuracy_min, collections[i].collection_filter.accuracy_max);
+                    } else {
+                        collections[i].default_name = format!("{}_{}_{}-{}", collections[i].default_name, args_per_value[0], collections[i].collection_filter.accuracy_min, collections[i].collection_filter.accuracy_max);
+                    }
+                },
+                "precision" => {
+                    collections[i].collection_filter.precision_min = safe_parse_f64(safe_get_string(&split_min_max, 0));
+                    collections[i].collection_filter.precision_max = safe_parse_f64(safe_get_string(&split_min_max, 1));
+                    if collections[i].default_name == String::default() {
+                        collections[i].default_name = format!("{}_{}-{}", args_per_value[0], collections[i].collection_filter.precision_min, collections[i].collection_filter.precision_max);
+                    } else {
+                        collections[i].default_name = format!("{}_{}_{}-{}", collections[i].default_name, args_per_value[0], collections[i].collection_filter.precision_min, collections[i].collection_filter.precision_max);
+                    }
+                },
+                "reaction" => {
+                    collections[i].collection_filter.reaction_min = safe_parse_f64(safe_get_string(&split_min_max, 0));
+                    collections[i].collection_filter.reaction_max = safe_parse_f64(safe_get_string(&split_min_max, 1));
+                    if collections[i].default_name == String::default() {
+                        collections[i].default_name = format!("{}_{}-{}", args_per_value[0], collections[i].collection_filter.reaction_min, collections[i].collection_filter.reaction_max);
+                    } else {
+                        collections[i].default_name = format!("{}_{}_{}-{}", collections[i].default_name, args_per_value[0], collections[i].collection_filter.reaction_min, collections[i].collection_filter.reaction_max);
+                    }
+                },
+                "memory"|"flashlight" => {
+                    collections[i].collection_filter.memory_min = safe_parse_f64(safe_get_string(&split_min_max, 0));
+                    collections[i].collection_filter.memory_max = safe_parse_f64(safe_get_string(&split_min_max, 1));
+                    if collections[i].default_name == String::default() {
+                        collections[i].default_name = format!("{}_{}-{}", args_per_value[0], collections[i].collection_filter.memory_min, collections[i].collection_filter.memory_max);
+                    } else {
+                        collections[i].default_name = format!("{}_{}_{}-{}", collections[i].default_name, args_per_value[0], collections[i].collection_filter.memory_min, collections[i].collection_filter.memory_max);
+                    }
+                },
+                _ => {}
+            }
+        }
+        i += 1;
+    }
+    
+    return collections;
+}
+
+fn read_maps(input_filepath: String) -> Vec<BeatmapData> {
+    let mut beatmaps_parsed: Vec<BeatmapData> = Default::default();
+
+    let file = match std::fs::File::open(&input_filepath) {
+        Ok(ok) => ok,
+        Err(_) => return vec![BeatmapData::default()]
+    };
+
+    let reader = std::io::BufReader::new(file);
+
+    let mut first_line: bool = true;
+    for (_index, line) in reader.lines().enumerate() {
+        let mut line_unwrap = match line {
+            Ok(ok) => ok,
+            Err(error) => { print!("osu!Skills rs: failed to parse file. Error: {}. Path: `{}`\n\n", error, input_filepath); return vec![BeatmapData::default()] }
+        };
+
+        if line_unwrap.contains(":") {
+            //not csv, each line's formatting must be removed
+            let line_headers: Vec<&str> = vec!["BeatmapID:","BeatmapsetID:","Md5:","Stamina:","Tenacity:","Agility:","Accuracy:","Precision:","Reaction:","Memory:","Streams:","Aim:","Flashlight:"];
+            for header in line_headers {
+                line_unwrap = line_unwrap.replace(header, "");
+            }
+
+            let mut char_vec: Vec<char> = Default::default();
+            for char in line_unwrap.chars() {
+                if char != ' ' {
+                    char_vec.push(char);
+                }
+            }
+            line_unwrap = char_vec.into_iter().collect();
+
+        } else if first_line == true {
+            //is csv, first line must be removed
+            first_line = false;
+            continue;
+        }
+
+        let line_split: Vec<&str> = line_unwrap.split(",").collect();
+        let beatmap: BeatmapData = BeatmapData { 
+            beatmap_id: safe_get_string(&line_split, 0), 
+            beatmap_set_id: safe_get_string(&line_split, 1), 
+            beatmap_md5: safe_get_string(&line_split, 2), 
+            stamina: safe_parse_f64(safe_get_string(&line_split, 3)), 
+            tenacity: safe_parse_f64(safe_get_string(&line_split, 4)), 
+            agility: safe_parse_f64(safe_get_string(&line_split, 5)), 
+            precision: safe_parse_f64(safe_get_string(&line_split, 6)), 
+            memory: safe_parse_f64(safe_get_string(&line_split, 7)), 
+            accuracy: safe_parse_f64(safe_get_string(&line_split, 8)), 
+            reaction: safe_parse_f64(safe_get_string(&line_split, 9)), 
+            title: safe_get_string(&line_split, 10), 
+            title_unicode: safe_get_string(&line_split, 11), 
+            artist: safe_get_string(&line_split, 12), 
+            artist_unicode: safe_get_string(&line_split, 13), 
+            version: safe_get_string(&line_split, 14), 
+        };
+        
+        beatmaps_parsed.push(beatmap);
+    }
+
+    return beatmaps_parsed;
+}
+
+#[derive(Default, Clone)]
+struct BeatmapData {
+    pub beatmap_id: String,
+    pub beatmap_set_id: String,
+    pub beatmap_md5: String,
+    pub title: String,
+    pub title_unicode: String,
+    pub artist: String,
+    pub artist_unicode: String,
+    pub version: String,
+
+    pub stamina: f64,
+    pub tenacity: f64,
+    pub agility: f64,
+    pub precision: f64,
+    pub memory: f64,
+    pub accuracy: f64,
+    pub reaction: f64
+}
+
+#[derive(Default, Clone)]
+struct Collection {
+    pub collection_name: String,
+    pub default_name: String,
+    pub beatmap_data: Vec<BeatmapData>,
+    pub collection_filter: Filter
+}
+
+#[derive(Clone)]
+struct Filter {
+    pub stamina_min: f64,
+    pub tenacity_min: f64,
+    pub agility_min: f64,
+    pub precision_min: f64,
+    pub memory_min: f64,
+    pub accuracy_min: f64,
+    pub reaction_min: f64,
+
+    pub stamina_max: f64,
+    pub tenacity_max: f64,
+    pub agility_max: f64,
+    pub precision_max: f64,
+    pub memory_max: f64,
+    pub accuracy_max: f64,
+    pub reaction_max: f64
+}
+
+impl Default for Filter {
+    fn default() -> Self {
+        Filter { 
+            stamina_min: (f64::MIN), tenacity_min: (f64::MIN), agility_min: (f64::MIN), precision_min: (f64::MIN), memory_min: (f64::MIN), accuracy_min: (f64::MIN), reaction_min: (f64::MIN), 
+            stamina_max: (f64::MAX), tenacity_max: (f64::MAX), agility_max: (f64::MAX), precision_max: (f64::MAX), memory_max: (f64::MAX), accuracy_max: (f64::MAX), reaction_max: (f64::MAX) 
+        }
+    }
+}
+
+fn safe_get_string(input: &Vec<&str>, index: usize) -> String {
+    let output = match input.get(index) {
+        Some(some) => some.to_string(),
+        None => "".to_string()
+    };
+    return output;
+}
+
+fn safe_parse_f64(input: String) -> f64 {
+    let output = match input.parse::<f64>() {
+        Ok(ok) => ok,
+        Err(error) => { print!("osu!Skills rs: failed to parse f64. Error: {}: `{}`\n\n", error, input); 0.0 }
+    };
+    return output;
+}
