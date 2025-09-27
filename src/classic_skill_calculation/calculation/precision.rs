4265891175e9cb1921ca708b341edf8c0d6e0570
kuuuube <hexagonisalie@gmail.com>
2025-09-26 22:51:53 -0400

    Implement agility cap

--- src/classic_skill_calculation/calculation/precision.rs
+++ src/classic_skill_calculation/calculation/precision.rs
@@ -6,8 +6,11 @@ pub fn calculate_precision(beatmap: &structs::Beatmap) -> f64 {
     let total_pow: f64 = beatmap.skill_calculation_vars.precision.total_pow;
     let agility_subtract: f64 = beatmap.skill_calculation_vars.precision.agility_subtract;
 
-    let scaled_agility: f64 =
-        f64::powf(beatmap.skills.agility + 1.0, agility_pow) - agility_subtract;
+    let scaled_agility: f64 = if beatmap.skills.agility > beatmap.skill_calculation_vars.precision.agility_limit {
+        1.0
+    } else {
+        f64::powf(beatmap.skills.agility + 1.0, agility_pow) - agility_subtract
+    };
 
     let precision: f64 =
         total_mult * f64::powf(scaled_agility * (beatmap.cs as i32 as f64), total_pow);
